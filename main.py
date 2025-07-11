ba7265a01861854bba7d9cdda7f6e2ad12372832
Olateru Olu <oluolateru@gmail.com>
2025-07-12 00:21:46 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from App.Class.Enemy import *
+
+enemy1 = Enemy()
+
+enemy1.type_of_enemy = 'Zombie'
+
+print(f"{enemy1.type_of_enemy} has health points {enemy1.health_points} and can do an attack damage of {enemy1.attack_damage}")
